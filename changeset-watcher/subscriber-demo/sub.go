04c0d08873c82ba49c2fc3da211f7a917c2a38e8
Darius Alter <darius.alter@stud.hs-hannover.de>
2022-02-21 10:44:21 +0100

    error handling for failed NATS-Server connection

--- changeset-watcher/subscriber-demo/sub.go
+++ changeset-watcher/subscriber-demo/sub.go
@@ -7,7 +7,12 @@ import (
 )
 
 func main() {
-	nc, _ := nats.Connect(nats.DefaultURL)
+	nc, err := nats.Connect(nats.DefaultURL)
+	defer nc.Close()
+
+	if err != nil {
+		fmt.Printf("Failed to connect to NATS-Server: %s \n", err.Error())
+	}
 
 	nc.Subscribe("foo", func(m *nats.Msg) {
 		fmt.Printf("Received a message: %s\n", string(m.Data))
