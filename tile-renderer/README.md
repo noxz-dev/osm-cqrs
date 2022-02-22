e2415838d39afe9e98fa9c4259bf448f63a4c210
James Neumann <41788164+JamesNeumann@users.noreply.github.com>
2022-02-22 22:41:02 +0100

    Added Imposm3 docs and basic docker setup

--- tile-renderer/README.md
+++ tile-renderer/README.md
@@ -2,6 +2,8 @@
 
 ## Useful command
 
+### Osmosis & osm2psql
+
 Prepare OsmChange for inserting into PostGIS
 
 `osmosis --read-xml-change 126.osc --simplify-change --write-xml-change out_2.xml`
@@ -14,4 +16,49 @@ osm2pgsql --append \
     --style openstreetmap-carto.style \ 
     --tag-transform-script openstreetmap-carto.lua \ 
     -d gis -H localhost -U renderer -W out_2.xml
+````
+
+### Imposm3
+
+Insert initial osm file with setup for continuous osm changeset update
+
+````shell
+imposm import \
+    -config config.json \ 
+    -read path_to_osm.osm.pbf \
+    -write -diff
+````
+
+Config file sums als configuration arguments together (
+see [Config file](https://imposm.org/docs/imposm3/latest/tutorial.html#config-file)). The`mapping.json` file contains
+the database schema (
+see [Data Mapping](https://imposm.org/docs/imposm3/latest/mapping.html)).
+
+````json5
+// config.json
+{
+  "cacedir": "./cache",
+  "connection": "postgis://postgres:password@localhost/postgres",
+  "mapping": "./mapping.json",
+  "diffdir": "./diff"
+}
+````
+
+To make the database available for continuous updates the tables must be made ready for production. They will be added
+to the public schema and the import schema will be removed.
+
+````shell
+imposm import -config config.json -deployproduction
+````
+
+Automatic updates can then be started with:
+
+````shell
+imposm run -config config.json
+````
+
+To import just a one or multiple specific change file run:
+
+````shell
+imposm diff -config config.json changes-1.osc.gz changes-2.osc.gz
 ````
--- tile-renderer/types/types.go
+++ tile-renderer/types/types.go
@@ -1,1 +1,71 @@
 package types
+
+type Osm struct {
+	Version   string      `xml:"version"`
+	ChageSets []ChangeSet `xml:"changeset"`
+}
+
+type ChangeSet struct {
+	Id         int     `xml:"id,attr"`
+	CreatedAt  string  `xml:"created_at,attr"`
+	NumChanges int     `xml:"num_changes,attr"`
+	MinLat     float32 `xml:"min_lat,attr"`
+	MaxLat     float32 `xml:"max_lat,attr"`
+	MinLong    float32 `xml:"min_lon,attr"`
+	MaxLong    float32 `xml:"max_lon,attr"`
+
+	Tags []Tag `xml:"tag"`
+}
+
+type Tag struct {
+	K string `xml:"k,attr"`
+	V string `xml:"v,attr"`
+}
+
+type OsmChange struct {
+	Version string   `xml:"version"`
+	Modify  []Action `xml:"modify"`
+	Create  []Action `xml:"create"`
+	Delete  []Action `xml:"delete"`
+}
+
+type Action struct {
+	Nodes     []Node     `xml:"node"`
+	Ways      []Way      `xml:"way"`
+	Relations []Relation `xml:"relation"`
+}
+
+type Node struct {
+	Id        int     `xml:"id,attr"`
+	Version   int     `xml:"version,attr"`
+	Timestamp string  `xml:"timestamp,attr"`
+	Lat       float32 `xml:"lat,attr"`
+	Lon       float32 `xml:"lon,attr"`
+	Tags      []Tag   `xml:"tag"`
+}
+
+type Way struct {
+	Id        int       `xml:"id,attr"`
+	Version   int       `xml:"version,attr"`
+	Timestamp string    `xml:"timestamp,attr"`
+	NodeRefs  []NodeRef `xml:"nd"`
+	Tags      []Tag     `xml:"tag"`
+}
+
+type NodeRef struct {
+	Ref int `xml:"ref,attr"`
+}
+
+type Relation struct {
+	Id        int      `xml:"id,attr"`
+	Version   int      `xml:"version,attr"`
+	Timestamp string   `xml:"timestamp,attr"`
+	Member    []Member `xml:"member"`
+	Tags      []Tag    `xml:"tag"`
+}
+
+type Member struct {
+	Type string `xml:"type,attr"`
+	Ref  int    `xml:"ref,attr"`
+	Role string `xml:"role,attr"`
+}
