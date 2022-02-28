258e6e7389271df29ef2cf46b42ab08ff61ce35b
herralter <dariusalter@mailbox.org>
2022-02-28 09:09:57 +0100

    Extract all change-methods from types.go

---
+++ changeset-watcher/types/change.go
@@ -0,0 +1,72 @@
+package types
+
+func (normalized OsmChangeNormalized) ExtractMissingNodes() (nodeIDs map[int]struct{}, missingNodes int, foundNodes int) {
+	missingNodes = 0
+	foundNodes = 0
+	nodeIDs = make(map[int]struct{})
+	normalized.Modify.extractMissingNodes(&nodeIDs, &missingNodes, &foundNodes)
+	//osmChangeNormalized.Delete.extractMissingNodes(&nodeIDs, &missingNodes, &foundNodes)
+	normalized.Create.extractMissingNodes(&nodeIDs, &missingNodes, &foundNodes)
+	return
+}
+
+func (normalized *OsmChangeNormalized) Reload() (err error) {
+
+	nodeIDs, _, _ := normalized.ExtractMissingNodes()
+	reloadedNodes, err := GetNodesByID(nodeIDs)
+	if err != nil {
+		return err
+	}
+	normalized.Reloaded.Nodes = reloadedNodes
+	return nil
+}
+
+func (normalized OsmChangeNormalized) Filter(nodeFilters []NodeFilter, wayFilters []WayFilter) OsmChangeNormalized {
+	usedNodes := make(map[int]struct{}, 0)
+	normalized.Create.FilterWays(wayFilters...)
+	normalized.Modify.FilterWays(wayFilters...)
+	normalized.Delete.FilterWays(wayFilters...)
+
+	normalized.Create.UsedNodes(&usedNodes)
+	normalized.Delete.UsedNodes(&usedNodes)
+	normalized.Modify.UsedNodes(&usedNodes)
+
+	normalized.Modify.UsedNodesByFilter(&usedNodes, nodeFilters...)
+	normalized.Delete.UsedNodesByFilter(&usedNodes, nodeFilters...)
+	normalized.Reloaded.UsedNodesByFilter(&usedNodes, nodeFilters...)
+	normalized.Create.UsedNodesByFilter(&usedNodes, nodeFilters...)
+
+	normalized.Create.DeleteAllNodesExcept(usedNodes)
+	normalized.Delete.DeleteAllNodesExcept(usedNodes)
+	normalized.Modify.DeleteAllNodesExcept(usedNodes)
+	normalized.Reloaded.DeleteAllNodesExcept(usedNodes)
+
+	return normalized
+
+}
+
+func (osmChange OsmChange) Normalize() OsmChangeNormalized {
+	return OsmChangeNormalized{
+		Modify: normalizeActionObject(osmChange.Modify),
+		Delete: normalizeActionObject(osmChange.Delete),
+		Create: normalizeActionObject(osmChange.Create),
+	}
+
+}
+
+func normalizeActionObject(actions []Action) (normalizedAction Action) {
+	ways := make([]Way, 0)
+	nodes := make([]Node, 0)
+	relations := make([]Relation, 0)
+
+	for _, action := range actions {
+		ways = append(ways, action.Ways...)
+		nodes = append(nodes, action.Nodes...)
+		relations = append(relations, action.Relations...)
+	}
+	return Action{
+		Ways:      ways,
+		Nodes:     nodes,
+		Relations: relations,
+	}
+}
--- changeset-watcher/types/types.go
+++ changeset-watcher/types/types.go
@@ -112,53 +112,6 @@ const (
 	CREATE_EVENT = "CREATE"
 )
 
-func (osmChange OsmChange) Normalize() OsmChangeNormalized {
-	return OsmChangeNormalized{
-		Modify: normalizeActionObject(osmChange.Modify),
-		Delete: normalizeActionObject(osmChange.Delete),
-		Create: normalizeActionObject(osmChange.Create),
-	}
-
-}
-
-func normalizeActionObject(actions []Action) (normalizedAction Action) {
-	ways := make([]Way, 0)
-	nodes := make([]Node, 0)
-	relations := make([]Relation, 0)
-
-	for _, action := range actions {
-		ways = append(ways, action.Ways...)
-		nodes = append(nodes, action.Nodes...)
-		relations = append(relations, action.Relations...)
-	}
-	return Action{
-		Ways:      ways,
-		Nodes:     nodes,
-		Relations: relations,
-	}
-}
-
-func (normalized OsmChangeNormalized) ExtractMissingNodes() (nodeIDs map[int]struct{}, missingNodes int, foundNodes int) {
-	missingNodes = 0
-	foundNodes = 0
-	nodeIDs = make(map[int]struct{})
-	normalized.Modify.extractMissingNodes(&nodeIDs, &missingNodes, &foundNodes)
-	//osmChangeNormalized.Delete.extractMissingNodes(&nodeIDs, &missingNodes, &foundNodes)
-	normalized.Create.extractMissingNodes(&nodeIDs, &missingNodes, &foundNodes)
-	return
-}
-
-func (normalized *OsmChangeNormalized) Reload() (err error) {
-
-	nodeIDs, _, _ := normalized.ExtractMissingNodes()
-	reloadedNodes, err := GetNodesByID(nodeIDs)
-	if err != nil {
-		return err
-	}
-	normalized.Reloaded.Nodes = reloadedNodes
-	return nil
-}
-
 func (way Way) HasTags(tags ...string) bool {
 	for _, tag := range tags {
 		_, err := way.GetTag(tag)
@@ -230,28 +183,4 @@ func GetNodesByID(nodeIDs map[int]struct{}) (nodes []Node, err error) {
 	}
 
 	return overpassAnswer.Nodes, nil
-}
-
-func (normalized OsmChangeNormalized) Filter(nodeFilters []NodeFilter, wayFilters []WayFilter) OsmChangeNormalized {
-	usedNodes := make(map[int]struct{}, 0)
-	normalized.Create.FilterWays(wayFilters...)
-	normalized.Modify.FilterWays(wayFilters...)
-	normalized.Delete.FilterWays(wayFilters...)
-
-	normalized.Create.UsedNodes(&usedNodes)
-	normalized.Delete.UsedNodes(&usedNodes)
-	normalized.Modify.UsedNodes(&usedNodes)
-
-	normalized.Modify.UsedNodesByFilter(&usedNodes, nodeFilters...)
-	normalized.Delete.UsedNodesByFilter(&usedNodes, nodeFilters...)
-	normalized.Reloaded.UsedNodesByFilter(&usedNodes, nodeFilters...)
-	normalized.Create.UsedNodesByFilter(&usedNodes, nodeFilters...)
-
-	normalized.Create.DeleteAllNodesExcept(usedNodes)
-	normalized.Delete.DeleteAllNodesExcept(usedNodes)
-	normalized.Modify.DeleteAllNodesExcept(usedNodes)
-	normalized.Reloaded.DeleteAllNodesExcept(usedNodes)
-
-	return normalized
-
 }
