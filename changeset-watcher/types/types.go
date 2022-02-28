36880eaa0605ddaa26f85264bbece27ee7ced5ef
herralter <dariusalter@mailbox.org>
2022-02-28 09:11:21 +0100

    Extract all way-methods from types.go

--- changeset-watcher/types/types.go
+++ changeset-watcher/types/types.go
@@ -112,27 +112,6 @@ const (
 	CREATE_EVENT = "CREATE"
 )
 
-func (way Way) HasTags(tags ...string) bool {
-	for _, tag := range tags {
-		_, err := way.GetTag(tag)
-		if err != nil {
-			return false
-		}
-	}
-	return true
-
-}
-
-func (way Way) GetTag(tagString string) (value string, err error) {
-	for _, tag := range way.Tags {
-		if tagString == tag.K {
-			return tag.V, nil
-		}
-	}
-	return "", errors.New("Tag " + tagString + " not found")
-
-}
-
 func (node Node) HasTags(tags ...string) bool {
 	for _, tag := range tags {
 		_, err := node.GetTag(tag)
