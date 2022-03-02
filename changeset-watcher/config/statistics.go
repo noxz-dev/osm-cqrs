cbaf84ddb35b16866bf7977d7218ab5b301ba9ae
herralter <dariusalter@mailbox.org>
2022-03-02 19:45:07 +0100

    überschriften für statistiken verbessert

--- changeset-watcher/config/statistics.go
+++ changeset-watcher/config/statistics.go
@@ -3,8 +3,8 @@ package config
 const (
 	DurationNodesReloading           = "duration for reloading nodes (in ms)"
 	NumberOfReloadedNodes            = "number of reloaded nodes"
-	DurationForRoutesFiltering       = "duration for Filtering for [" + RoutingSubject + "] (in ms)"
-	DurationForSearchFiltering       = "duration for Filtering for [" + SearchSubject + "] (in ms)"
+	DurationForRoutesFiltering       = "duration for Filtering for subject [" + RoutingSubject + "] (in ms)"
+	DurationForSearchFiltering       = "duration for Filtering for subject [" + SearchSubject + "] (in ms)"
 	DurationChangSetDownload         = "duration of download (in ms)"
 	NumberOfIncomingElements         = "number of incoming elements"
 	NumberOfPublishedElements        = "number of published elements for subject [" + AllSubject + "]"
