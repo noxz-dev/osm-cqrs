8d51b35771c33bcbc772537438efd2a259345c3f
herralter <dariusalter@mailbox.org>
2022-03-10 21:07:02 +0100

    "Alte" publish-Methoden entfernt

--- changeset-watcher/main.go
+++ changeset-watcher/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
@@ -267,110 +266,6 @@ func reloadNodes(osmNormalized *types.OsmChangeNormalized) {
 	logger.Info("missing nodes reloaded")
 }
 
-func sendSearchChangesets(nc *nats.Conn, normalized types.OsmChangeNormalized, wg *sync.WaitGroup) {
-	defer wg.Done()
-	err := stat.StartTimer(config.DurationForSearchFiltering)
-	if err != nil {
-		logger.Error(err.Error())
-	}
-	searchPayload := generateSearchEventPayload(normalized)
-	publishEvent(nc, config.SearchSubject, searchPayload, cloudevents.ApplicationJSON)
-	logIfFailing(stat.StopTimerAndSetDuration(config.DurationForSearchFiltering))
-	logIfFailing(stat.SetValue(config.NumberOfPublishedSearchElements, strconv.Itoa(searchPayload.Size())))
-
-}
-
-func sendRoutingChangesets(nc *nats.Conn, normalized types.OsmChangeNormalized, wg *sync.WaitGroup) {
-	defer wg.Done()
-	logIfFailing(stat.StartTimer(config.DurationForRoutesFiltering))
-
-	logger.Info("Anzahl Nodes vor dem Routing Filter: ", len(normalized.Create.Nodes), len(normalized.Reloaded.Nodes))
-	streets := normalized.Filter([]types.NodeFilter{}, []types.WayFilter{types.NewWayFilter("highway")})
-	logger.Info("Anzahl Nodes nach dem Routing Filter: ", len(streets.Create.Nodes), len(streets.Reloaded.Nodes))
-
-	createAction := types.Action{
-		Nodes:     append(streets.Create.Nodes, streets.Reloaded.Nodes...),
-		Ways:      append(streets.Create.Ways, streets.Reloaded.Ways...),
-		Relations: append(streets.Create.Relations, streets.Reloaded.Relations...),
-	}
-
-	xmlContent := types.OsmChangeNormalizedXML{
-		Create: createAction,
-		Delete: streets.Delete,
-		Modify: streets.Modify,
-	}
-
-	xmlData, err := xml.MarshalIndent(xmlContent, " ", "    ")
-	if err != nil {
-		logger.Error(err.Error())
-	}
-	xmlData = []byte(xml.Header + string(xmlData))
-
-	var b bytes.Buffer
-	w := gzip.NewWriter(&b)
-
-	_, err = w.Write(xmlData)
-	if err != nil {
-		logger.Error(err.Error())
-		return
-	}
-
-	err = w.Close()
-	if err != nil {
-		logger.Error(err.Error())
-		return
-	}
-
-	zippedBytes := b.Bytes()
-
-	publishEvent(nc, config.RoutingSubject, zippedBytes, "text/plain")
-	logIfFailing(stat.StopTimerAndSetDuration(config.DurationForRoutesFiltering))
-	logIfFailing(stat.SetValue(config.NumberOfPublishedRoutingElements, strconv.Itoa(streets.Size())))
-}
-
-func SendAllChangesets(nc *nats.Conn, normalized types.OsmChangeNormalized, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	createAction := types.Action{
-		Nodes:     append(normalized.Create.Nodes, normalized.Reloaded.Nodes...),
-		Ways:      append(normalized.Create.Ways, normalized.Reloaded.Ways...),
-		Relations: append(normalized.Create.Relations, normalized.Reloaded.Relations...),
-	}
-
-	xmlContent := types.OsmChangeNormalizedXML{
-		Create: createAction,
-		Modify: normalized.Modify,
-		Delete: normalized.Delete,
-	}
-
-	xmlData, err := xml.MarshalIndent(xmlContent, " ", "    ")
-	if err != nil {
-		logger.Error(err.Error())
-	}
-	xmlData = []byte(xml.Header + string(xmlData))
-
-	var b bytes.Buffer
-	w := gzip.NewWriter(&b)
-
-	_, err = w.Write(xmlData)
-	if err != nil {
-		logger.Error(err.Error())
-		return
-	}
-
-	err = w.Close()
-	if err != nil {
-		logger.Error(err.Error())
-		return
-	}
-
-	zippedBytes := b.Bytes()
-
-	publishEvent(nc, config.AllSubject, zippedBytes, "text/plain")
-	logIfFailing(stat.SetValue(config.NumberOfPublishedElements, strconv.Itoa(normalized.Size())))
-
-}
-
 func publishEvent(nc *nats.Conn, subject string, payload interface{}, contentType string) {
 	event, err := utils.CreateEvent("ChangesetWatcher", payload, subject, contentType)
 	if err != nil {
@@ -390,82 +285,6 @@ func publishEvent(nc *nats.Conn, subject string, payload interface{}, contentTyp
 	if err != nil {
 		logger.Error("failed to publish new change set to subject ["+subject+"]: ", err.Error())
 	}
-}
-
-func publishEventGziped(nc *nats.Conn, subject string, payload interface{}) {
-	event, err := utils.CreateEvent("ChangesetWatcher", payload, subject, cloudevents.ApplicationXML)
-	if err != nil {
-		logger.Error("cloudevents wrapper could not be created: ", err.Error())
-		return
-	}
-	eventBytes, err := json.Marshal(event)
-	if err != nil {
-		logger.Error("Event could not be serialized", err.Error())
-		return
-	}
-
-	var b bytes.Buffer
-	w := gzip.NewWriter(&b)
-
-	w.Write(eventBytes)
-	w.Close()
-
-	zippedBytes := b.Bytes()
-
-	logger.Info("publishing new changeset to " + subject + " ...")
-	err = nc.Publish(subject, zippedBytes)
-	if err != nil {
-		logger.Error("failed to publish new change set to subject ["+subject+"]: ", err.Error())
-	}
-}
-
-func generateSearchEventPayload(normalized types.OsmChangeNormalized) types.SearchPayload {
-	//TODO: Nodes-Filter nach Vorstellung entfernen. Ist hier nicht notwendig.
-	buildings := normalized.Filter(
-		[]types.NodeFilter{
-			types.NewNodeFilter("building", "name"),
-			types.NewNodeFilter("building", "addr:street"),
-			types.NewNodeFilter("amenity", "name"),
-			types.NewNodeFilter("tourism", "name"),
-		},
-		[]types.WayFilter{
-			types.NewWayFilter("building", "name"),
-			types.NewWayFilter("building", "addr:street"),
-			types.NewWayFilter("building", "addr:housenumber"),
-			types.NewWayFilter("amenity", "name"),
-			types.NewWayFilter("tourism", "name"),
-		})
-
-	tmp := append(buildings.Create.Nodes, buildings.Modify.Nodes...)
-	tmp = append(tmp, buildings.Delete.Nodes...)
-	tmp = append(tmp, buildings.Reloaded.Nodes...)
-
-	// modifySearchPoints := reduceWaysToSearchPoints(buildings.Modify.Ways, append(buildings.Modify.Nodes, buildings.Reloaded.Nodes...))
-	// createSearchPoints := reduceWaysToSearchPoints(buildings.Create.Ways, append(buildings.Create.Nodes, buildings.Reloaded.Nodes...))
-	// deleteSearchPoints := reduceWaysToSearchPoints(buildings.Delete.Ways, append(buildings.Delete.Nodes, buildings.Reloaded.Nodes...))
-
-	modifySearchPoints := reduceWaysToSearchPoints(buildings.Modify.Ways, tmp)
-	createSearchPoints := reduceWaysToSearchPoints(buildings.Create.Ways, tmp)
-	deleteSearchPoints := reduceWaysToSearchPoints(buildings.Delete.Ways, tmp)
-	points := normalized.Filter(
-		[]types.NodeFilter{
-			types.NewNodeFilter("building", "name"),
-			types.NewNodeFilter("addr:housenumber"),
-			types.NewNodeFilter("addr:street"),
-			types.NewNodeFilter("amenity", "name"),
-			types.NewNodeFilter("tourism", "name"),
-		},
-		[]types.WayFilter{})
-	modifySearchPoints = append(modifySearchPoints, reduceNodesToSearchPoints(points.Modify.Nodes)...)
-	createSearchPoints = append(createSearchPoints, reduceNodesToSearchPoints(points.Create.Nodes)...)
-	deleteSearchPoints = append(deleteSearchPoints, reduceNodesToSearchPoints(points.Delete.Nodes)...)
-
-	payload := types.SearchPayload{
-		Modify: modifySearchPoints,
-		Create: createSearchPoints,
-		Delete: deleteSearchPoints,
-	}
-	return payload
 }
 
 func reduceWaysToSearchPoints(ways []types.Way, nodes []types.Node) []types.SearchPoint {
