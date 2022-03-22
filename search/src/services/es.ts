d2632d2b0e9b15426b141aa94b913d582edf5dad
noxz-dev <finn.beer@gmail.com>
2022-03-22 19:24:04 +0100

    cleaned docker compose, added ngram filter

--- search/src/services/es.ts
+++ search/src/services/es.ts
@@ -7,10 +7,32 @@ try {
   const resp = await client.indices.create(
     {
       index: 'osm',
+      settings: {
+        max_ngram_diff: 20,
+        analysis: {
+          filter: {
+            ngram_filter: {
+              type: 'ngram',
+              min_gram: 2,
+              max_gram: 20,
+            },
+          },
+          analyzer: {
+            ngram_analyzer: {
+              type: 'custom',
+              tokenizer: 'standard',
+              filter: ['lowercase', 'ngram_filter'],
+            },
+          },
+        },
+      },
       mappings: {
         properties: {
           name: {
             type: 'text',
+            term_vector: 'yes',
+            analyzer: 'ngram_analyzer',
+            search_analyzer: 'standard',
           },
           location: {
             type: 'geo_point',
